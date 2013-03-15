0f4e73517030781edaf73452c636eb6082e0f320
unknown <hyunho63.shin@Shin-PC.LGE.NET>
2013-03-15 16:52:00 +0900

    4

--- src/kr/co/androider/spring3/hello/controller/HelloWorldController.java
+++ src/kr/co/androider/spring3/hello/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package kr.co.androider.spring3.hello;
+package kr.co.androider.spring3.hello.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
