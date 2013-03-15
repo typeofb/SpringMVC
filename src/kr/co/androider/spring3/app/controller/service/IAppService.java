3d2813972e645d1271e7564370b5542a707c6bfc
unknown <hyunho63.shin@Shin-PC.LGE.NET>
2013-03-15 17:41:47 +0900

    app service delete

--- src/kr/co/androider/spring3/app/controller/service/IAppService.java
+++
@@ -1,7 +0,0 @@
-package kr.co.androider.spring3.app.controller.service;
-
-import kr.co.androider.spring3.app.controller.bean.AppBean;
-
-public interface IAppService {
-    public AppBean changeInfo(AppBean appBean);
-}
