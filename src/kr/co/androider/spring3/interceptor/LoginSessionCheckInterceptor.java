ed85a1f620514083e00e934f324fccc06098bd7c
unknown <hyunho63.shin@Shin-PC.LGE.NET>
2013-03-18 11:27:55 +0900

    interceptors

---
+++ src/kr/co/androider/spring3/interceptor/LoginSessionCheckInterceptor.java
@@ -0,0 +1,20 @@
+package kr.co.androider.spring3.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginSessionCheckInterceptor extends HandlerInterceptorAdapter {
+    
+    private Logger logger = Logger.getLogger(getClass());
+
+    public boolean preHandle(HttpServletRequest request,
+            HttpServletResponse response, Object handler) throws Exception{
+        
+        logger.debug("##### interceptor #####");
+        
+        return true;
+    }
+}
