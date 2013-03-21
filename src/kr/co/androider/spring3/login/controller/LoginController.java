dc2542f587b04f96a58c282158807fe5df23727d
unknown <hyunho63.shin@Shin-PC.LGE.NET>
2013-03-21 09:33:48 +0900

    login modify

--- src/kr/co/androider/spring3/login/controller/LoginController.java
+++ src/kr/co/androider/spring3/login/controller/LoginController.java
@@ -16,14 +16,12 @@ public class LoginController {
 	@RequestMapping("/login")
 	public void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
 	    
-	    HttpSession session = request.getSession();
-	    
-	    session.setAttribute("token", request.getParameter("username"));
-	    
 	    response.setContentType("text/html; charset=utf-8");
 	    PrintWriter out = response.getWriter();
 	    out.println("<script>");
 	    if (request.getParameter("username").equals("typeofb")) {
+	        HttpSession session = request.getSession();
+	        session.setAttribute("token", request.getParameter("username"));
 	        out.println("location.href='" + request.getContextPath() + "/hello.do" + "';");
 	    } else {
             out.println("alert('Failed to login. The user account does not exist');");
