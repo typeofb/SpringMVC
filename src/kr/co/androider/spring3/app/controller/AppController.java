b00f9eed4a57e1602afe78691bd826ea46b0bd05
unknown <hyunho63.shin@Shin-PC.LGE.NET>
2013-03-27 17:24:38 +0900

    sql insert

--- src/kr/co/androider/spring3/app/controller/AppController.java
+++ src/kr/co/androider/spring3/app/controller/AppController.java
@@ -1,8 +1,9 @@
 package kr.co.androider.spring3.app.controller;
 
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.List;
+import java.util.UUID;
 
 import kr.co.androider.spring3.app.controller.bean.AppBean;
 import kr.co.androider.spring3.app.controller.dao.AppDao;
@@ -22,17 +23,31 @@ public class AppController extends SqlSessionDaoSupport {
     public ModelAndView app() {
         logger.info("console - debug level /app!");
         
+        AppDao appDao = getSqlSession().getMapper(AppDao.class);
+        
+        // SELECT
         Map<String, String> map = new HashMap<String, String>();
         map.put("appPkgName", "com.lge.lglink.group");
         map.put("appName", "Link Group");
-        
-        AppDao appDao = getSqlSession().getMapper(AppDao.class);
         List<AppBean> list = appDao.retrieveApp(map);
         for (int i = 0; i < list.size(); i++) {
             logger.debug(list.get(i).getAppId());
             logger.debug(list.get(i).getAppPkgName());
             logger.debug(list.get(i).getAppSize());
         }
+        
+        // INSERT
+        AppBean talkData = new AppBean();
+        talkData.setAppId(UUID.randomUUID().toString());
+        talkData.setAppName("Link Talk");
+        talkData.setAppPkgName("com.lge.lglink.talk");
+        talkData.setAppVerCode("100");
+        talkData.setAppVerName("1.0.100 Beta");
+        talkData.setUseFlag("Y");
+        talkData.setGenUsrId("cpmappuser");
+        talkData.setLastChgUsrId("cpmappadmin");
+        appDao.createApp(talkData);
+        
         
         ModelAndView mv = new ModelAndView();
         mv.setViewName("app");
--- src/kr/co/androider/spring3/app/controller/dao/AppDao.java
+++ src/kr/co/androider/spring3/app/controller/dao/AppDao.java
@@ -8,4 +8,6 @@ import kr.co.androider.spring3.app.controller.bean.AppBean;
 public interface AppDao {
 //    public List<AppBean> retrieveApp(@Param("appPkgName") String appPkgName, @Param("appName") String appName);
     public List<AppBean> retrieveApp(Map<String, String> map);
+    
+    public void createApp(AppBean data);
 }
