671afe261b09589e648b7bd375cb15575c505459
typeofb <typeofb@gmail.com>
2013-09-12 10:57:24 +0900

    패키지 경로 수정

--- src/kr/co/androider/spring3/contact/service/IContactService.java
+++
@@ -1,7 +0,0 @@
-package kr.co.androider.spring3.contact.service;
-
-import kr.co.androider.spring3.contact.vo.ContactVo;
-
-public interface IContactService {
-    public ContactVo changeInfo(ContactVo ContactVo);
-}
