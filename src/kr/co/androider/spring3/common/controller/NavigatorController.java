2e411e963708fc7fb2d807c4c98b841af9a79cfb
typeofb <typeofb@gmail.com>
2013-12-13 09:05:22 +0900

    navi dao 추가

--- src/kr/co/androider/spring3/common/controller/NavigatorController.java
+++ src/kr/co/androider/spring3/common/controller/NavigatorController.java
@@ -2,11 +2,11 @@ package kr.co.androider.spring3.common.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.co.androider.spring3.common.dao.NavigatorDao;
 import kr.co.androider.spring3.common.vo.NavigatorVO;
 
 import org.springframework.stereotype.Controller;
@@ -16,7 +16,6 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class NavigatorController {
 	
-	@SuppressWarnings("rawtypes")
 	@RequestMapping("/navigator")
 	public ModelAndView menu(HttpServletRequest request, HttpServletResponse response) {
 		
@@ -24,52 +23,52 @@ public class NavigatorController {
 		System.out.println(request.getParameter("parentMenuId"));
 		
 		String targetController = null;
-		List<Map> menu = new ArrayList<Map>();
-		List<Map> sidebar = new ArrayList<Map>();
+		List<NavigatorVO> menu = new ArrayList<NavigatorVO>();
+		List<NavigatorVO> sidebar = new ArrayList<NavigatorVO>();
 		
 		// 초기 화면
 		if (request.getParameter("menuId") == null && request.getParameter("parentMenuId") == null) {
-			menu = new NavigatorVO().retrieveMenu();
+			menu = new NavigatorDao().retrieveMenu();
 			
-			List<Map> temp = new NavigatorVO().retrieveSidebar();
+			List<NavigatorVO> temp = new NavigatorDao().retrieveSidebar();
 			for (int i = 0; i < temp.size(); i++) {
-				if ("0001".equals(temp.get(i).get("parentMenuId"))) {
+				if (temp.get(i).getParentMenuId() == 1) {
 					sidebar.add(temp.get(i));
-					if ("1001".equals(temp.get(i).get("menuId"))) {
-						targetController = temp.get(i).get("controllerName").toString();
+					if (temp.get(i).getMenuId() == 1001) {
+						targetController = temp.get(i).getControllerName();
 					}
 				}
 			}
 		}
 		
 		// 헤더 메뉴 클릭
-		else if (request.getParameter("parentMenuId") == null || request.getParameter("parentMenuId").equals("")) {
-			menu = new NavigatorVO().retrieveMenu();
+		else if (Integer.parseInt(request.getParameter("parentMenuId")) == 0) {
+			menu = new NavigatorDao().retrieveMenu();
 			
-			List<Map> temp = new NavigatorVO().retrieveSidebar();
+			List<NavigatorVO> temp = new NavigatorDao().retrieveSidebar();
 			for (int i = 0; i < temp.size(); i++) {
-				if (request.getParameter("menuId").equals(temp.get(i).get("parentMenuId"))) {
+				if (Integer.parseInt(request.getParameter("menuId")) == temp.get(i).getParentMenuId()) {
 					sidebar.add(temp.get(i));
 				}
 			}
 			
 			for (int i = 0; i < menu.size(); i++) {
-				if (request.getParameter("menuId").equals(menu.get(i).get("menuId"))) {
-					targetController = menu.get(i).get("controllerName").toString();
+				if (Integer.parseInt(request.getParameter("menuId")) == menu.get(i).getMenuId()) {
+					targetController = menu.get(i).getControllerName();
 				}
 			}
 		}
 		
 		// 사이드바 클릭
 		else {
-			menu = new NavigatorVO().retrieveMenu();
+			menu = new NavigatorDao().retrieveMenu();
 			
-			List<Map> temp = new NavigatorVO().retrieveSidebar();
+			List<NavigatorVO> temp = new NavigatorDao().retrieveSidebar();
 			for (int i = 0; i < temp.size(); i++) {
-				if (request.getParameter("parentMenuId").equals(temp.get(i).get("parentMenuId"))) {
+				if (Integer.parseInt(request.getParameter("parentMenuId")) == temp.get(i).getParentMenuId()) {
 					sidebar.add(temp.get(i));
-					if (request.getParameter("menuId").equals(temp.get(i).get("menuId"))) {
-						targetController = temp.get(i).get("controllerName").toString();
+					if (Integer.parseInt(request.getParameter("menuId")) == temp.get(i).getMenuId()) {
+						targetController = temp.get(i).getControllerName();
 					}
 				}
 			}
--- src/kr/co/androider/spring3/common/dao/NavigatorDao.java
+++ src/kr/co/androider/spring3/common/dao/NavigatorDao.java
@@ -1,5 +1,87 @@
 package kr.co.androider.spring3.common.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.androider.spring3.common.vo.NavigatorVO;
+
 public class NavigatorDao {
 
+	public List<NavigatorVO> retrieveMenu() {
+		NavigatorVO navi1 = new NavigatorVO();
+		navi1.setMenuId(1);
+		navi1.setMenuName("User Mgmt");
+		navi1.setParentMenuId(0);
+		navi1.setControllerName("hello.do");
+		NavigatorVO navi2 = new NavigatorVO();
+		navi2.setMenuId(2);
+		navi2.setMenuName("Service Mgmt");
+		navi2.setParentMenuId(0);
+		navi2.setControllerName("app.do");
+		NavigatorVO navi3 = new NavigatorVO();
+		navi3.setMenuId(3);
+		navi3.setMenuName("Service Monitoring");
+		navi3.setParentMenuId(0);
+		navi3.setControllerName("contact.do");
+		NavigatorVO navi4 = new NavigatorVO();
+		navi4.setMenuId(4);
+		navi4.setMenuName("Common Mgmt");
+		navi4.setParentMenuId(0);
+		navi4.setControllerName("report.do");
+		
+		List<NavigatorVO> resultList = new ArrayList<NavigatorVO>();
+		resultList.add(navi1);
+		resultList.add(navi2);
+		resultList.add(navi3);
+		resultList.add(navi4);
+		return resultList;
+	}
+	
+	public List<NavigatorVO> retrieveSidebar() {
+		NavigatorVO navi1 = new NavigatorVO();
+		navi1.setMenuId(1001);
+		navi1.setMenuName("Say Hello");
+		navi1.setParentMenuId(1);
+		navi1.setControllerName("hello.do");
+		NavigatorVO navi2 = new NavigatorVO();
+		navi2.setMenuId(1002);
+		navi2.setMenuName("Retrieve App");
+		navi2.setParentMenuId(2);
+		navi2.setControllerName("app.do");
+		NavigatorVO navi3 = new NavigatorVO();
+		navi3.setMenuId(1003);
+		navi3.setMenuName("Contact");
+		navi3.setParentMenuId(3);
+		navi3.setControllerName("contact.do");
+		NavigatorVO navi4 = new NavigatorVO();
+		navi4.setMenuId(1004);
+		navi4.setMenuName("Report Page");
+		navi4.setParentMenuId(4);
+		navi4.setControllerName("report.do");
+		NavigatorVO navi5 = new NavigatorVO();
+		navi5.setMenuId(1005);
+		navi5.setMenuName("SMS 발송");
+		navi5.setParentMenuId(1);
+		navi5.setControllerName("sms.do");
+		NavigatorVO navi6 = new NavigatorVO();
+		navi6.setMenuId(1006);
+		navi6.setMenuName("사원 정보");
+		navi6.setParentMenuId(1);
+		navi6.setControllerName("employee.do");
+		NavigatorVO navi7 = new NavigatorVO();
+		navi7.setMenuId(1007);
+		navi7.setMenuName("Highcharts");
+		navi7.setParentMenuId(4);
+		navi7.setControllerName("highcharts.do");
+		
+		List<NavigatorVO> resultList = new ArrayList<NavigatorVO>();
+		resultList.add(navi1);
+		resultList.add(navi2);
+		resultList.add(navi3);
+		resultList.add(navi4);
+		resultList.add(navi5);
+		resultList.add(navi6);
+		resultList.add(navi7);
+		return resultList;
+	}
 }
