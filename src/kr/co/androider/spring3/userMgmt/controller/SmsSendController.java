c6905596b9b6dd9bebab2da572f8e10517d42283
typeofb <typeofb@gmail.com>
2014-02-20 13:05:11 +0900

    SMS 발송 - 다중 DB 설정

--- src/kr/co/androider/spring3/userMgmt/controller/SmsSendController.java
+++ src/kr/co/androider/spring3/userMgmt/controller/SmsSendController.java
@@ -3,9 +3,13 @@ package kr.co.androider.spring3.userMgmt.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
+import kr.co.androider.spring3.userMgmt.dao.SmsSendDaoImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +17,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class SmsSendController {
 	
+	@Autowired
+	private SmsSendDaoImpl dao;
+	
 	@RequestMapping("/smsSend")
 	public void smsSend(HttpServletResponse response,
 			@RequestParam HashMap<Object, Object> param,
 			@RequestParam(value="USR_ID_RCV", required=true) String[] usrIdRcv) throws IOException {
 
 		System.out.println("log - smsSend");
+		
+		// kepcoDB
+		List<HashMap<String, Object>> list = dao.selectUser();
+		System.out.println(list);
+		
 		System.out.println(param);
 		System.out.println(usrIdRcv);
 		for (int i = 0; i < usrIdRcv.length; i++) {
